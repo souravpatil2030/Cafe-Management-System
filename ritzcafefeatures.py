81f1cbc3222aa828ed5877bb69737c6f9530d713
sp715991 <116378172+sp715991@users.noreply.github.com>
2022-10-23 21:59:03 +0530

    Create ritzcafefeatures.py

---
+++ ritzcafefeatures.py
@@ -0,0 +1,4 @@
+print("Welcome to the project the features are as follows: ")
+print("Specially designed to handle multiple clients simultaneously")
+print("Better and Secured UI")
+print("Customer Oriented")
